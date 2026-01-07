9e022c4a84c2f0fd6086ce8e664733995eed775b
Saad1379 <sheikh.saad.riaz@gmail.com>
2026-01-07 05:47:31 +0500

    Refactor database schema: remove deprecated tables and columns, implement tiered pricing for activities, and add new description templates for orders.

--- packages/db/prisma/migrations/20251029105225_user/migration.sql
+++
@@ -1,6 +0,0 @@
--- AlterTable
-ALTER TABLE "employees" ALTER COLUMN "firstName" DROP NOT NULL,
-ALTER COLUMN "lastName" DROP NOT NULL;
-
--- AlterTable
-ALTER TABLE "users" ALTER COLUMN "email" DROP NOT NULL;
